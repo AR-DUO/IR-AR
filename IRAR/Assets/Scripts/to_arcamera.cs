37bb239caea630c8e0a8c4bf918f5f80ff5bb66e
kimchaeyeon51 <61893327+kimchaeyeon51@users.noreply.github.com>
2020-03-21 12:06:14 +0900

    임시 메인화면 완성

---
+++ IRAR/Assets/Scripts/to_arcamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class to_arcamera : MonoBehaviour
+{
+
+   public void Move_to_cam()
+    {
+        SceneManager.LoadScene("AR_Cam");
+        SceneManager.LoadScene("ar_scene");
+    }
+}
